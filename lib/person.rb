be7b860e0a80009896907cb6a4e5d54e8c2889d5
rose534 <wambuirose534@gmail.com>
2023-02-11 20:17:52 +0300

    lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
-# your code goes here
+class Person
+  attr_reader :name, :bank_account, :happiness, :hygiene
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def bank_account=(value)
+    @bank_account = value
+  end
+  
+  def happiness=(value)
+    if value > 10
+      @happiness = 10
+    elsif value >= 0
+      @happiness = value
+    else
+      @happiness = 0
+    end
+  end
+  
+  def hygiene=(value)
+    if value > 10
+      @hygiene = 10
+    elsif value >= 0
+      @hygiene = value
+    else
+      @hygiene = 0
+    end
+  end
+end
