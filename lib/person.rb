39c16869d8481e3558d3d03c73605d22ff75dde6
rose534 <wambuirose534@gmail.com>
2023-02-11 21:56:15 +0300

    lab

--- lib/person.rb
+++ lib/person.rb
@@ -23,12 +23,56 @@ class Person
   end
   
   def hygiene=(value)
-    if value > 10
-      @hygiene = 10
-    elsif value >= 0
-      @hygiene = value
-    else
-      @hygiene = 0
-    end
+  if value >= 0
+    @hygiene = [value, 10].min
+  else
+    @hygiene = 0
   end
+  end
+  
+  def clean?
+    @hygiene > 7
+  end
+  
+  def happy?
+    @happiness > 7
+  end
+  
+  def get_paid(amount)
+    @bank_account += amount
+    "all about the benjamins"
+  end
+  
+ def take_bath
+  self.hygiene = [@hygiene + 4, 10].min
+  "♪ Rub-a-dub just relaxing in the tub ♫"
+end
+  
+ def work_out
+self.happiness = [@happiness + 2, 10].min
+@hygiene -= 3
+"♪ another one bites the dust ♫"
+end
+  
+ def call_friend(friend)
+self.happiness = [@happiness + 3, 10].min
+friend.happiness = [friend.happiness + 3, 10].min
+"Hi #{friend.name}! It's #{@name}. How are you?"
+end
+  
+  def start_conversation(friend, topic)
+if topic == "politics"
+self.happiness = [@happiness - 2, 0].max
+friend.happiness = [friend.happiness - 2, 0].max
+"blah blah partisan blah lobbyist"
+elsif topic == "weather"
+self.happiness = [@happiness + 1, 10].min
+friend.happiness = [friend.happiness + 1, 10].min
+"blah blah sun blah rain"
+else
+"blah blah blah blah blah"
+end
+end
+
+
 end
